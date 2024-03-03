10a0c2f87a3ae55b96752e041f1ae94b36a84afb
gyuwon <gyuwoneee@gmail.com>
2024-03-03 15:54:01 +0900

    ControllerExceptionHandler updated

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.Exception.CustomValidationException;
+import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,8 @@ import java.util.Map;
 @ControllerAdvice // 모든 Exception 다 낚아챔!!
 public class ControllerExceptionHandler {
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e){
-        return e.getErrorMap();
+    public CMRespDto<?> validationException(CustomValidationException e){
+        return new CMRespDto(-1,e.getMessage(), e.getErrorMap());
     }
 
 }
