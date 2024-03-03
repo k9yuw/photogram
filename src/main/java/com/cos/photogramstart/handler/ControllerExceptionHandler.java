9d5e6d03bf5d8c3c56bb2c516ee01088217abcb2
gyuwon <gyuwoneee@gmail.com>
2024-03-03 16:39:33 +0900

    exception 부분 CMRespDto에서 Script 방식으로 수정

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.Exception.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,8 +13,10 @@ import java.util.Map;
 @ControllerAdvice // 모든 Exception 다 낚아챔!!
 public class ControllerExceptionHandler {
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e){
-        return new CMRespDto(-1,e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e){
+        // 클라이언트한테 응답할 때는 Script가 좋음.
+        // Ajax 통신이나 Android 통신을 할 때는 CMResponseDto가 좋음
+        return Script.back(e.getErrorMap().toString());
     }
 
 }
